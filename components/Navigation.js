97d5eb8ad33dca0f8338d4a390172e492b472bfe
RetroRabiul <rabiulcaffe@gmail.com>
2026-04-17 18:38:45 +0600

    Import useEffect in Navigation to fix SSR build

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Navigation.module.css';
