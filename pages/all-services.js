0d6a73f3f1ae3fad42239f7a724245999be4bfe2
RetroRabiul <rabiulcaffe@gmail.com>
2026-04-20 02:45:20 +0600

    removed title

--- pages/all-services.js
+++ pages/all-services.js
@@ -165,7 +165,6 @@ export default function AllServices() {
 
 const DETAILS = {
   101: {
-    title: 'Bathroom Deep Cleaning',
     items: [
       { label: 'Only Bathroom = Floor + Wall + Single Basin + Single Pan/Commode + Mirror + ventilator', price: '' },
       { label: 'Only Bathroom', price: '1000' },
@@ -175,7 +174,6 @@ const DETAILS = {
     ]
   },
   102: {
-    title: 'Kitchen Deep Cleaning Service',
     items: [
       { label: 'Only Kitchin = Floor + Wall + Sink + Outside Cabinet + Inside Window + Exist fan', price: '' },
       { label: 'Only Kitchen', price: '1500' },
@@ -184,7 +182,6 @@ const DETAILS = {
     ]
   },
   103: {
-    title: 'Floor Deep Cleaning',
     items: [
       { label: 'Tiles', price: '৳3/Sft' },
       { label: 'Mosaic', price: '৳4/Sft' },
@@ -193,7 +190,6 @@ const DETAILS = {
     ]
   },
   104: {
-    title: 'Full Home Deep Cleaning',
     items: [
       { label: '800-1000 (2 bathroom+1 Balcony)', price: '৳4,000' },
       { label: '1001-1300 (3 Bathroom+2 balcony)', price: '৳5,000' },
@@ -203,14 +199,12 @@ const DETAILS = {
     ]
   },
   105: {
-    title: 'Window Cleaning',
     items: [
       { label: 'Inside Window Cleaning (Minimum 5 Window)', price: '৳200' },
       { label: 'Outside Window Cleaning (Minimum 5 Window)', price: '৳800' }
     ]
   },
   106: {
-    title: 'Thai Glass Cleaning',
     items: [
       { label: 'Indoor Glass', price: '৳4/Sft' },
       { label: 'Outdoor Glass', price: '৳8/Sft' }
