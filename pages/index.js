7c81f92bdabb51d950d8fb3b9526defb60910a05
RetroRabiul <rabiulcaffe@gmail.com>
2026-04-25 20:24:34 +0600

    UI: add seeAllTile class (blue background, white label)

--- pages/index.js
+++ pages/index.js
@@ -139,7 +139,7 @@ export default function Home() {
                   return (
                     <div
                       key="see-all"
-                      className={styles.categoryTile}
+                      className={[styles.categoryTile, styles.seeAllTile].filter(Boolean).join(' ')}
                       role="button"
                       tabIndex={0}
                       onClick={() => router.push('/all-services')}
